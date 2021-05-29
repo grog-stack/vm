b3152e75a16738e3e546fffa3e28fdb43b47c023
Martín Gaspar Straus <martinstraus@gmail.com>
2021-05-29 20:47:56 -0300

    Most basic run loop.

--- grog.c
+++ grog.c
@@ -3,12 +3,17 @@
 #define REGISTERS_COUNT 16
 #define MEMORY_SIZE 64*1024 // 64K should be enough...
 
+// Instruction set
+
+#define HCF 0x00 // Halt and catch fire
+
 typedef unsigned char byte;
+typedef unsigned int address;
 
 struct GrogVM {
     byte registers[REGISTERS_COUNT];
     byte memory[MEMORY_SIZE];
-    byte pc; // program counter
+    address pc; // program counter
 };
 
 typedef struct GrogVM GrogVM;
@@ -18,10 +23,17 @@ void run(GrogVM *vm) {
     printf("--------------------\n");
     printf("%d registers\n", REGISTERS_COUNT);
     printf("%d addressable bytes in memory\n", MEMORY_SIZE);
+    printf("\nRunning...\n");
+    byte instruction = vm->memory[vm->pc];
+    while (instruction != HCF) {
+
+    }
+    printf("Halt and catch fire!\n");
 }
 
 int main(int argc, char **argv)
 {
     GrogVM vm = {};
+    vm.memory[0] = HCF;
     run(&vm);
 }
