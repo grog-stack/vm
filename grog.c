aa6fa5cca30f3e2ae0ec9141e12e96df3dfbff53
Martín Gaspar Straus <martinstraus@gmail.com>
2021-05-29 20:28:57 -0300

    Added struct for virtual machine.

--- grog.c
+++ grog.c
@@ -1,6 +1,27 @@
 #include "stdio.h"
 
+#define REGISTERS_COUNT 16
+#define MEMORY_SIZE 64*1024 // 64K should be enough...
+
+typedef unsigned char byte;
+
+struct GrogVM {
+    byte registers[REGISTERS_COUNT];
+    byte memory[MEMORY_SIZE];
+    byte pc; // program counter
+};
+
+typedef struct GrogVM GrogVM;
+
+void run(GrogVM *vm) {
+    printf("Grog Virtual Machine\n");
+    printf("--------------------\n");
+    printf("%d registers\n", REGISTERS_COUNT);
+    printf("%d addressable bytes in memory\n", MEMORY_SIZE);
+}
+
 int main(int argc, char **argv)
 {
-    printf("Grog Virtual Machine\n");
+    GrogVM vm = {};
+    run(&vm);
 }
